997ce3890c2844befc1238c6dc098c6c2327b05f
MeerBiene <b.buhles@gmail.com>
2020-07-25 01:31:04 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log("working")
+const express = require('express')
+const hbs = require('express-handlebars')
+const { createClient } = require('redis')
+
+const redisclient = createClient();
